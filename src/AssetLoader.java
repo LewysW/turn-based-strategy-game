85d5eb8a77ba29dfecb504e8025e7d1bd0d44110
lewys <lewysward99@gmail.com>
2020-06-08 00:45:39 +0100

    Added code to extract image files from zip archives and to delete them after they are loaded into memory

--- src/AssetLoader.java
+++ src/AssetLoader.java
@@ -1,9 +1,7 @@
 import javax.imageio.ImageIO;
 import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,12 +10,37 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class AssetLoader {
+    /**
+     * Size of the buffer to read/write data
+     */
+    private static final int BUFFER_SIZE = 4096;
+
+
     public LinkedHashMap<String, BufferedImage> loadImages() {
         LinkedHashMap<String, BufferedImage> images = new LinkedHashMap<>();
         try {
             List<File> filesInFolder = Files.walk(Paths.get("resources/"))
+                    .filter(Files::isRegularFile)
+                    .map(Path::toFile)
+                    .collect(Collectors.toList());
+
+            //Unzips image files
+            for (File file : filesInFolder) {
+                if (file.toString().endsWith(".zip")) {
+                    int indexOfExt = file.toString().indexOf(".");
+                    int indexOfSlash = file.toString().lastIndexOf("/");
+                    String continent = file.toString().substring(indexOfSlash + 1, indexOfExt);
+                    System.out.println("Continent: " + continent);
+                    unzip(file.toString(), "resources/" + continent + "/");
+                }
+            }
+
+            //Crawls image files
+            filesInFolder = Files.walk(Paths.get("resources/"))
                     .filter(Files::isRegularFile)
                     .map(Path::toFile)
                     .collect(Collectors.toList());
@@ -30,6 +53,7 @@ public class AssetLoader {
                     String territoryName = file.toString().substring(indexOfSlash + 1, indexOfExt);
                     System.out.println("Name: " + territoryName);
                     images.put(territoryName, ImageIO.read(file));
+                    file.delete();
                 }
             }
 
@@ -38,6 +62,53 @@ public class AssetLoader {
         }
 
         return images;
+    }
+
+    /**
+     * Extracts a zip file specified by the zipFilePath to a directory specified by
+     * destDirectory (will be created if does not exists)
+     * @param zipFilePath
+     * @param destDirectory
+     * @throws IOException
+     */
+    public void unzip(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+
+    /**
+     * Extracts a zip entry (file entry)
+     * @param zipIn
+     * @param filePath
+     * @throws IOException
+     */
+    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
     }
 
     public LinkedHashMap<String, Path2D> loadBorders() {
